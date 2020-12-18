53e2d37378a79661fca753b18eb492a0b0a6e201
Marina Zabrodska <marinazabrodska8@gmail.com>
2020-12-19 01:58:04 +0200

    add function completeTask& changed lockalstorage

--- src/js/script.js
+++ src/js/script.js
@@ -4,12 +4,17 @@ let inputBtn = document.querySelector('.input__btn'),
  toDo = document.querySelector('.maine');
 
 // create a function that will be add new task in the list  
-let toDoList;
-console.log(toDoList);
-// check localstorage 
-!localStorage.toDoList ? toDoList = [] : toDoList = JSON.parse(localStorage.getItem('toDoList'));
+let toDoList =[];
+
+let toDoItemEl =[];
+
+if (localStorage.getItem('todo')) {
+    toDoList = JSON.parse(localStorage.getItem('todo'));
+    outRender();
+}
 
 inputBtn.addEventListener('click', function(){
+
     let newToDo = {
         todo: inputUser.value,
         checked: false,
@@ -17,47 +22,42 @@ inputBtn.addEventListener('click', function(){
     };
 
     toDoList.push(newToDo);
-
-    console.log(toDoList);
-    addLocal();
     outRender();
+    // create localstorage 
+    localStorage.setItem('todo', JSON.stringify(toDoList));
 });
 
-
-// create a function complited task
-
-/* const checked = index => {
+const completeTask = index => {
     console.log(index);
-    toDoList[index].completed = !toDoList[index].completed;
-}
- */
-// create localstorage 
-let addLocal = () => {
-    localStorage.setItem('toDoList', JSON.stringify(toDoList));
+    toDoList[index].checked = !toDoList[index].checked;
+    if(toDoList[index].checked) {
+        toDoItemEl[index].classList.add('.checked');
+    } else {
+        toDoItemEl[index].classList.remove('.checked');
+    }
+    outRender();
+    localStorage.setItem('todo', JSON.stringify(toDoList));
 }
 
 // create a function that will be render all tasks from toDoList
-function outRender(){
+function outRender() {
     let displayMassage ='';
-    if (toDoList.length > 0 ) {
+    if (toDoList.length > 0) {
         toDoList.forEach(function(item, i){
-
             displayMassage += `
-            <li class='maine__item'>
-                <input class='item__input' type='checkbox' id='item_${i}' ${item.checked ? 'checked' : ''}>
+            <li class='maine__item ${item.checked ? 'checked' : ''}'>
+                <input onclick= 'completeTask(${i})' class='item__input' type='checkbox' id='item_${i}' ${item.checked ? 'checked' : ''}>
                 <label for='item_${i}' class='item__label'>${item.todo}</label>
                 <button class='main__btn'>Delete</button>
             </li>
-            `;
-    /* console.log(displayMassage); */
-
+            `; 
         });
+        toDo.innerHTML = displayMassage;
+        toDoItemEl = document.querySelectorAll('.maine__item');
     }
-    toDo.innerHTML = displayMassage;
 } 
-outRender();
 
-toDo.addEventListener('change', function(event) {
+/* toDo.addEventListener('change', function(event) {
     let idInput = event.target.getAttribute('id');
     let forLabel = toDo.querySelector('[for='+ idInput +']');
     let valueLable = forLabel.innerHTML;
@@ -71,7 +71,7 @@ toDo.addEventListener('change', function(event) {
             localStorage.setItem('toDoList', JSON.stringify(toDoList));
         }
         });
-});
+}); */
 
 
 
