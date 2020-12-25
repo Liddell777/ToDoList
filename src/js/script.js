86818cf617d28f0983a7fb17a11452c06191c6b3
Marina Zabrodska <marinazabrodska8@gmail.com>
2020-12-25 18:22:16 +0200

    fixed mistakes for PR, added function for delete tasks, fixed css style

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,8 @@
+const plusButton = document.getElementById('plus_button');
+const taskInput =  document.getElementById('task_input');
+const taskList = document.querySelector('.maine');
 
-let inputBtn = document.querySelector('.input__btn'),
- inputUser =  document.querySelector('.input__user'),
- toDo = document.querySelector('.maine');
 
-// create a function that will be add new task in the list  
 let toDoList =[];
 
 let toDoItemEl =[];
@@ -13,66 +12,69 @@ if (localStorage.getItem('todo')) {
     outRender();
 }
 
-inputBtn.addEventListener('click', function(){
-
+// create a function that will be add new task in the list
+plusButton.addEventListener('click', function(){
+    
     let newToDo = {
-        todo: inputUser.value,
+        todo: taskInput.value,
         checked: false,
         important: false
     };
 
     toDoList.push(newToDo);
     outRender();
-    // create localstorage 
-    localStorage.setItem('todo', JSON.stringify(toDoList));
+    updateStorage();
+    // clear input before type
+    taskInput.value =''; 
 });
 
+// create a function that will be checked task
 const completeTask = index => {
-    console.log(index);
     toDoList[index].checked = !toDoList[index].checked;
-    if(toDoList[index].checked) {
-        toDoItemEl[index].classList.add('.checked');
-    } else {
-        toDoItemEl[index].classList.remove('.checked');
-    }
+    let addTaskClass = (toDoList[index].checked) ? toDoItemEl[index].classList.add('.checked') : 
+    toDoItemEl[index].classList.remove('.checked');
     outRender();
+    updateStorage();
+}
+
+// create a function that will be delete task 
+const deleteTask = index => {
+    toDoList.splice(index,1);
+    outRender();
+    updateStorage();
+}
+
+
+// create a function that will be add a localStorage 
+function updateStorage() {
     localStorage.setItem('todo', JSON.stringify(toDoList));
 }
 
 // create a function that will be render all tasks from toDoList
 function outRender() {
-    let displayMassage ='';
+    let displayedMessage = '';
     if (toDoList.length > 0) {
         toDoList.forEach(function(item, i){
-            displayMassage += `
-            <li class='maine__item ${item.checked ? 'checked' : ''}'>
+            displayedMessage += `
+            <li class='maine__item '>
                 <input onclick= 'completeTask(${i})' class='item__input' type='checkbox' id='item_${i}' ${item.checked ? 'checked' : ''}>
-                <label for='item_${i}' class='item__label'>${item.todo}</label>
-                <button class='main__btn'>Delete</button>
-            </li>
+                <label for='item_${i}' class='item__label ${item.checked ? 'checked' : ''}'>
+                    ${item.todo}
+                </label>
+                <button onclick= 'deleteTask(${i})' class='main__btn'>Delete</button>     
+            </li> 
             `; 
         });
-        toDo.innerHTML = displayMassage;
-        toDoItemEl = document.querySelectorAll('.maine__item');
+        taskList.innerHTML = displayedMessage;
+        toDoItemEl = document.getElementsByClassName('maine__item');
     }
 } 
 
-/* toDo.addEventListener('change', function(event) {
-    let idInput = event.target.getAttribute('id');
-    let forLabel = toDo.querySelector('[for='+ idInput +']');
-    let valueLable = forLabel.innerHTML;
-
-    toDoList.forEach(function(item) {
-        if (item.todo === valueLable) {
-            item.checked = !item.checked;
-            if (item.checked === true) {
-                forLabel.classList.add('item__label-thr');
-            }
-            localStorage.setItem('toDoList', JSON.stringify(toDoList));
-        }
-        });
-}); */
-
-
-
-
+// create a function that will be filter completed task and send them down the list
+/* const filterTask = () => {
+    const activeTasks = toDoItemEl.length && toDoItemEl.filter(item => item.checked == false);
+    const checkedTasks = toDoItemEl.length && toDoItemEl.filter(item => item.checked == true);
+    toDoList = [...activeTasks,...checkedTasks];
+    console.log(activeTasks,checkedTasks);
+}
+ */
